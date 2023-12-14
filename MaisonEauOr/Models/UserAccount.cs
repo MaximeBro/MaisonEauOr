529392364595eef45d7ac6aa72fe4499247b439b
MaximeBro <maximebrochard.pro@gmail.com>
2023-12-14 16:38:16 +0100

    Adds user profile page

--- MaisonEauOr/Models/UserAccount.cs
+++ MaisonEauOr/Models/UserAccount.cs
@@ -10,6 +10,7 @@ public class UserAccount
     public string? PhoneNumber { get; set; }
     public string? Address { get; set; }
     public string? Town { get; set; }
+    public string? Country { get; set; }
     public int PostalCode { get; set; }
     public UserRole Role { get; set; }
     public string Password { get; set; }
